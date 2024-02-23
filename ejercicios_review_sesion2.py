c73e0e35d60c1a7edb2d9106cc387d6cfc456ab6
valen4312 <kevindaza13@gmail.com>
2024-02-23 13:00:58 -0500

    realizacion ejercicio 3

---
+++ ejercicios_review_sesion2.py
@@ -0,0 +1,41 @@
+#ejercicio sobra variables y tipos de datos
+#ejercicio1
+Nombre = "Mateo Montoya Ospina"
+#ejercicio2
+Edad = 17
+Peso = 50,"kg"
+Altura = 1.70,"meters"
+#ejercicio3
+radio = int(input("ingrese el radio del circulo a calculat"))
+areaCirculo = radio**2
+print("el area de su circulo es:areaCirculo")
+#ejercicio4
+#ejercicio5
+#ejercicio6
+#ejercicio7
+#ejercicio8
+#ejercicio9
+#ejercicio10
+#ejercicio11
+#ejercicio12
+#ejercicio13
+#ejercicio14
+#ejercicio15
+
+#ejercicios para identificar tipos de datos
+
+#ejercicio1
+#ejercicio2
+#ejercicio3
+#ejercicio4
+#ejercicio5
+#ejercicio6
+#ejercicio7
+#ejercicio8
+#ejercicio9
+#ejercicio10
+#ejercicio11
+#ejercicio12
+#ejercicio13
+#ejercicio14
+#ejercicio15
