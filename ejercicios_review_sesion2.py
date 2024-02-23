bf7b71976b23af7f9043619e0c7b18267904c768
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-02-23 14:03:04 -0500

    ejercicio4

--- ejercicios_review_sesion2.py
+++ ejercicios_review_sesion2.py
@@ -10,7 +10,8 @@ radio = int(input("ingrese el radio del circulo a calcular: "))
 areaCirculo = radio*radio
 print(f"el area de su circulo es: {areaCirculo} ")
 #ejercicio4
-
+float(lista = [1,1.2,2,2.3])
+random_number= (lista)
 #ejercicio5
 #ejercicio6
 #ejercicio7
