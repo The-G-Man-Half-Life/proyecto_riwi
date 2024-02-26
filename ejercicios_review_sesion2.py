b8cd24e47ef8ebf036beadf7e21b8bcd386f2523
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-02-26 13:57:45 -0500

    avance hasta ejercicio 5

--- ejercicios_review_sesion2.py
+++ ejercicios_review_sesion2.py
@@ -1,6 +1,6 @@
 #ejercicio sobra variables y tipos de datos
 #ejercicio1
-Nombre = "Mateo Montoya Ospina"
+"""Nombre = "Mateo Montoya Ospina"
 #ejercicio2
 Edad = 17
 Peso = 50,"kg"
@@ -8,18 +8,28 @@ Altura = 1.70,"meters"
 #ejercicio3
 radio = int(input("ingrese el radio del circulo a calcular: "))
 areaCirculo = radio*radio
-print(f"el area de su circulo es: {areaCirculo} ")
+print(f"el area de su circulo es: {areaCirculo} ")"""
 #ejercicio4
-Farenheit = "Farenheit" or "farenheit"
+"""Farenheit = "Farenheit" or "farenheit"
 Celsius = "Celsius" or "celsius"
 tipo_temperatura = input("ingrese el tipo de temperatura a convertir(Farenheit o Celsius): ")
 valor = float(input("ingrese el valor: "))
 
 if tipo_temperatura == Farenheit:
-    ecuacion_farenheit_celsius = valor-32/1.8
+    ecuacion_farenheit_celsius = (valor-32)/(1.8)
+    print(f"Este es valor en grados Celsius: {ecuacion_farenheit_celsius}°C")
+else:
+    ecuacion_celsius_farenheit = (valor*1.8)+32
+    print(f"Este es el valor en grados Farenheit: {ecuacion_celsius_farenheit}°F")"""
 #ejercicio5
-    float(lista = [1,1.2,2,2.3])
-random_number= ()
+hora = int(input("Escriba la hora actual (en formato militar): "))
+minuto = int(input("escriba el minuto actual: "))
+sol = ((hora in range(6,16))) or (hora == 17 and minuto in range(0,31))
+if sol == True:
+    print("Aun hay sol, salte de la pc y toca cesped")
+else:
+    print("Hay luna, no salgas que te asaltan los malandros")
+    
 #ejercicio6
 #ejercicio7
 #ejercicio8
@@ -37,6 +47,10 @@ random_number= ()
 #ejercicio2
 #ejercicio3
 #ejercicio4
+import random
+lista = [1,1.2,2,2.3]
+random_number= random.choice(lista)
+print(random_number)
 #ejercicio5
 #ejercicio6
 #ejercicio7
@@ -50,3 +64,5 @@ random_number= ()
 #ejercicio15
 #hola
 #hola2
+
+#datos -credenciales
