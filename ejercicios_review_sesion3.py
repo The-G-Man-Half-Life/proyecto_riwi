ef79aaa3be8e937917a128d2b0553362b5fcc2c7
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-02-28 14:01:50 -0500

    Nose

---
+++ ejercicios_review_sesion3.py
@@ -0,0 +1,74 @@
+datoGeneral = int(input("Ingrese un numero entero: "))
+datoGeneral2 = int(input("Ingrese otro numero entero: "))
+#problema 1
+sumar = datoGeneral + datoGeneral2
+print(sumar)
+
+#problema 2
+restar = datoGeneral  - datoGeneral2
+print(restar)
+
+#problema 3
+multiplicar = datoGeneral * datoGeneral2
+print(multiplicar)
+
+#problema 4
+division= datoGeneral/datoGeneral2
+residuo = datoGeneral%datoGeneral2
+print(f"El resultado de la division es: {division}")
+print(f"El residuo especificamente es: {residuo}")
+
+#problema 5 y problema 6
+division= datoGeneral2/datoGeneral
+residuo = datoGeneral2%datoGeneral
+print(f"El resultado de la division es: {division}")
+print(f"El residuo especificamente es: {residuo}")
+
+#problema 7
+exponenciacion = datoGeneral**datoGeneral2
+print(exponenciacion)
+
+#problema 8
+if datoGeneral==datoGeneral2:
+    print("ambos son iguales")
+else:
+    print("ambos numeros son distintos")
+#problema 9
+
+if datoGeneral==datoGeneral2:
+    print("ambos son iguales")
+else:
+    print("ambos numeros son desiguales")
+
+#problema 10
+verificacion = datoGeneral < datoGeneral2
+if verificacion == True:
+    print("El primero dato es menor al segundo")
+elif verificacion == False:
+    print("El segundo dato es menor al primero")
+    
+#problema 11
+verificacion = datoGeneral > datoGeneral2
+if verificacion == True:
+    print("El primero dato es mayor al segundo")
+elif verificacion == False:
+    print("El segundo dato es mayor al primero")
+    
+#problema 12
+verificacion = datoGeneral <= datoGeneral2
+if verificacion == True:
+    print("El primero dato es menor o igual al segundo")
+elif verificacion == False:
+    print("El segundo dato es menor o igual al primero")
+#problema 13
+verificacion = datoGeneral >= datoGeneral2
+if verificacion == True:
+    print("El primero dato es mayor al segundo")
+elif verificacion == False:
+    print("El segundo dato es mayor al primero")
+#problema 14
+#problema 15
+#problema 16
+#Ejercicio 1
+#Ejercicio 2
+#Ejercicio 3
